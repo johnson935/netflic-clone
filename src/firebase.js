8130b0de15726c555dd0a184d0ebeb27b7b1e27e
johnson935 <33596645+johnson935@users.noreply.github.com>
2021-08-31 23:19:03 +0100

    Delete firebase.js

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAS10gU1DlaeBsbpGPXDHiBckzYx21-aPQ",
-    authDomain: "netflix-clone-b1239.firebaseapp.com",
-    projectId: "netflix-clone-b1239",
-    storageBucket: "netflix-clone-b1239.appspot.com",
-    messagingSenderId: "495121939890",
-    appId: "1:495121939890:web:d89d9d26975849f6abf8ab"
-  };
